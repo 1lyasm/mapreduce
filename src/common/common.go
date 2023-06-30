10feaac02c430a48d6133bc495214687e836f5fb
1lyasm <il.mystafa@gmail.com>
2023-06-30 18:03:57 +0400

    add exes to main package

--- src/common/common.go
+++ src/common/common.go
@@ -1,10 +1,17 @@
 package common
 
 const (
-	IpAddr string = "127.0.0.1"
+	IpAddr string = "192.168.1.7"
 	Port   string = "1234"
 )
 
-func MakeFailMsg(funcName string) string {
-	return funcName + " failed"
+func MakeFailMsg(funcName string, e error) string {
+	return funcName + " failed: " + e.Error()
+}
+
+type ArgType struct {
+	Arg int
+}
+type ReplyType struct {
+	Reply int
 }
