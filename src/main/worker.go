573e28eeb95a832c3dd6d9f9301a1585d19f6cd8
1lyasm <il.mystafa@gmail.com>
2023-07-03 23:45:57 +0400

    add sendHeartbeat

--- src/main/worker.go
+++ src/main/worker.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"io"
 	"log"
 	. "mapreduce/common"
 	"net/rpc"
+	"os"
+	"sync"
+	"time"
 )
 
-func callRegWorker(client *rpc.Client) {
+func callRegWorker(client *rpc.Client) int {
 	arg := &RegWorkerArg{}
 	var reply RegWorkerReply
 	e := client.Call("Workers.RegWorker", arg, &reply)
@@ -14,12 +18,37 @@ func callRegWorker(client *rpc.Client) {
 		Fail("client.Call", e)
 	}
 	log.Printf("callRegWorker")
+	return reply.Id
+}
+
+func callUpdateLastSeen(client *rpc.Client, id int) {
+	arg := &UpdateLastSeenArg{Id: id, LastSeen: time.Now()}
+	var reply UpdateLastSeenReply
+	e := client.Call("Workers.UpdateLastSeen", arg, &reply)
+	if e != nil {
+		Fail("client.Call", e)
+	}
+	log.Printf("callUpdateLastSeen")
+}
+
+func sendHeartbeat(client *rpc.Client, id int) {
+	for {
+		callUpdateLastSeen(client, id)
+		time.Sleep(time.Second)
+	}
 }
 
 func main() {
-	client, e := rpc.DialHTTP("tcp", IpAddr+":"+Port)
+	if len(os.Args) < 2 || os.Args[1] != "-v" {
+		log.SetOutput(io.Discard)
+	}
+	client, e := rpc.DialHTTP("tcp", HostIp+":"+Port)
 	if e != nil {
 		Fail("rpc.DialHTTP", e)
 	}
-	callRegWorker(client)
+	Id := callRegWorker(client)
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go sendHeartbeat(client, Id)
+	wg.Wait()
 }
