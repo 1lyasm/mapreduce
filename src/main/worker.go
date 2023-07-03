e27a9aec32c79ce2f024413cf6632b72b08be452
1lyasm <il.mystafa@gmail.com>
2023-07-04 02:33:27 +0400

    add break to sendHeartbeat and restart to main

--- src/main/worker.go
+++ src/main/worker.go
@@ -10,45 +10,76 @@ import (
 	"time"
 )
 
+type HeartbeatStatus struct {
+	mu   sync.Mutex
+	code int
+}
+
+func (status *HeartbeatStatus) getCodeAtomic() int {
+	status.mu.Lock()
+	defer status.mu.Unlock()
+	return status.code
+}
+
 func callRegWorker(client *rpc.Client) int {
 	arg := &RegWorkerArg{}
 	reply := RegWorkerReply{}
 	e := client.Call("Workers.RegWorker", arg, &reply)
 	if e != nil {
-		Fail("client.Call", e)
+		Fail("callRegWorker: client.Call", e)
 	}
-	log.Printf("callRegWorker")
+	log.Printf("callRegWorker: registered, id: %d", reply.Id)
 	return reply.Id
 }
 
-func callUpdateLastSeen(client *rpc.Client, id int) {
+func callUpdateLastSeen(client *rpc.Client, id int) int {
 	arg := &UpdateLastSeenArg{Id: id, LastSeen: time.Now()}
 	reply := UpdateLastSeenReply{}
 	e := client.Call("Workers.UpdateLastSeen", arg, &reply)
 	if e != nil {
-		Fail("client.Call", e)
+		Fail("callUpdateLastSeen: client.Call", e)
 	}
-	log.Printf("callUpdateLastSeen")
+	if reply.ErrorCode == 1 {
+		return 1
+	}
+	log.Printf("callUpdateLastSeen: updated")
+	return 0
 }
 
-func sendHeartbeat(client *rpc.Client, id int) {
+func sendHeartbeat(client *rpc.Client, id int, wg *sync.WaitGroup,
+	status *HeartbeatStatus) {
 	for {
-		callUpdateLastSeen(client, id)
+		e := callUpdateLastSeen(client, id)
+		if e == 1 {
+			log.Printf("sendHeartbeat: could not send")
+			break
+		}
 		time.Sleep(time.Second)
 	}
+	wg.Done()
+	status.code = 1
 }
 
 func main() {
 	if len(os.Args) < 2 || os.Args[1] != "-v" {
 		log.SetOutput(io.Discard)
 	}
-	client, e := rpc.DialHTTP("tcp", HostIp+":"+Port)
-	if e != nil {
-		Fail("rpc.DialHTTP", e)
+	status := HeartbeatStatus{code: 1}
+	for status.getCodeAtomic() == 1 {
+		client, e := rpc.DialHTTP("tcp", HostIp+":"+Port)
+		if e != nil {
+			Fail("rpc.DialHTTP", e)
+		}
+		Id := callRegWorker(client)
+		wg := sync.WaitGroup{}
+		wg.Add(1)
+		go func() {
+			status.mu.Lock()
+			status.code = 0
+			sendHeartbeat(client, Id, &wg, &status)
+			status.mu.Unlock()
+			log.Print("main: restarting worker")
+		}()
+		wg.Wait()
 	}
-	Id := callRegWorker(client)
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-	go sendHeartbeat(client, Id)
-	wg.Wait()
 }
