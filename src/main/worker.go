7dede4fc7bb32254dc1af65bfe185d61db32f386
1lyasm <il.mystafa@gmail.com>
2023-07-05 23:12:29 +0400

    seperate heartbeat and getTasks thread logic

--- src/main/worker.go
+++ src/main/worker.go
@@ -6,86 +6,90 @@ import (
 	. "mapreduce/common"
 	"net/rpc"
 	"os"
+	"os/exec"
 	"sync"
 	"time"
 )
 
-type HeartbeatStatus struct {
-	mu   sync.Mutex
-	code int
-}
-
-func (status *HeartbeatStatus) getCodeAtomic() int {
-	status.mu.Lock()
-	defer status.mu.Unlock()
-	return status.code
-}
-
-func callRegWorker(client *rpc.Client) int {
+func regWorker(client *rpc.Client) int {
 	arg := &RegWorkerArg{}
 	reply := RegWorkerReply{}
 	e := client.Call("Workers.RegWorker", arg, &reply)
 	if e != nil {
-		Fail("callRegWorker: client.Call", e)
+		Fail("regWorker: client.Call", e)
 	}
-	log.Printf("callRegWorker: registered, id: %d", reply.Id)
+	log.Printf("regWorker: registered, id: %d", reply.Id)
 	return reply.Id
 }
 
-func callUpdateLastSeen(client *rpc.Client, id int) int {
+func updateLastSeen(client *rpc.Client, id int) int {
 	arg := &UpdateLastSeenArg{Id: id, LastSeen: time.Now()}
 	reply := UpdateLastSeenReply{}
 	e := client.Call("Workers.UpdateLastSeen", arg, &reply)
 	if e != nil {
-		Fail("callUpdateLastSeen: client.Call", e)
+		Fail("updateLastSeen: client.Call", e)
 	}
 	if reply.ErrorCode == 1 {
 		return 1
 	}
-	log.Printf("callUpdateLastSeen: updated")
+	log.Printf("updateLastSeen: updated")
 	return 0
 }
 
-func sendHeartbeat(client *rpc.Client, id int, wg *sync.WaitGroup,
-	status *HeartbeatStatus) {
+func heartbeat(client *rpc.Client, id int) {
 	for {
-		e := callUpdateLastSeen(client, id)
+		e := updateLastSeen(client, id)
 		if e == 1 {
-			log.Printf("sendHeartbeat: could not send")
-			break
+			return
 		}
 		time.Sleep(time.Second)
 	}
-	status.code = 1
 }
 
-func requestTasks(wg *sync.WaitGroup) {
-	wg.Done()
+func getTasks() {
+}
+
+func incAtom(a *int, mu *sync.Mutex) {
+	mu.Lock()
+	*a = *a + 1
+	mu.Unlock()
 }
 
 func main() {
 	if len(os.Args) < 2 || os.Args[1] != "-v" {
 		log.SetOutput(io.Discard)
 	}
-	status := HeartbeatStatus{code: 1}
-	for status.getCodeAtomic() == 1 {
-		client, e := rpc.DialHTTP("tcp", HostIp+":"+Port)
+	client, e := rpc.DialHTTP("tcp", HostIp+":"+Port)
+	if e != nil {
+		Fail("main: rpc.DialHTTP", e)
+	}
+	id := regWorker(client)
+	ch := make(chan int)
+	doneCnt := 0
+	muDone := sync.Mutex{}
+	go func() {
+		heartbeat(client, id)
+		log.Print("main: heartbeat error, restarting worker")
+		ch <- 1
+		incAtom(&doneCnt, &muDone)
+	}()
+	go func() {
+		getTasks()
+		log.Print("main: completed tasks")
+		ch <- 2
+		incAtom(&doneCnt, &muDone)
+	}()
+	muDone.Lock()
+	done := <-ch
+	if doneCnt == 2 {
+		done = <-ch
+	}
+	muDone.Unlock()
+	if done == 1 {
+		cmd := exec.Command("/usr/bin/bash", "-c", "./worker")
+		e = cmd.Start()
 		if e != nil {
-			Fail("rpc.DialHTTP", e)
+			Fail("main: Start", e)
 		}
-		Id := callRegWorker(client)
-		wg := sync.WaitGroup{}
-		wg.Add(1)
-		go func() {
-			status.mu.Lock()
-			status.code = 0
-			sendHeartbeat(client, Id, &wg, &status)
-			status.mu.Unlock()
-			log.Print("main: restarting worker")
-			wg.Done()
-		}()
-		wg.Add(1)
-		go requestTasks(&wg)
-		wg.Wait()
 	}
 }
