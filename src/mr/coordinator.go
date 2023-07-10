30eb3a502266bf89f6cd8f7ec037aee192da55dc
1lyasm <il.mystafa@gmail.com>
2023-07-10 21:55:28 +0400

    complete getT

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -45,9 +45,12 @@ type Tasks struct {
 }
 
 type Coordinator struct {
-	workers *Workers
-	tasks   *Tasks
-	nRed    int
+	workers  *Workers
+	tasks    *Tasks
+	nRed     int
+	muRedCnt sync.Mutex
+	redCnt   int
+	fCnt     int
 }
 
 func (c *Coordinator) server() {
@@ -64,14 +67,21 @@ func (c *Coordinator) server() {
 }
 
 func (c *Coordinator) Done() bool {
-	ret := false
-	return ret
+	c.muRedCnt.Lock()
+	defer c.muRedCnt.Unlock()
+	return c.redCnt == c.nRed
 }
 
-func (tasks *Tasks) fill(files []string) {
+func (tasks *Tasks) fill(files []string, nRed int) {
 	for i := 0; i < len(files); i += 1 {
+		log.Printf("fill: new task with file: %s", files[i])
 		tasks.List = append(tasks.List,
 			Task{File: files[i], Stat: TaskFree, Type: TaskM, Num: i})
+	}
+	for i := 0; i < nRed; i += 1 {
+		tasks.List = append(tasks.List,
+			Task{Stat: TaskFree, Type: TaskR,
+				Num: i})
 	}
 }
 
@@ -120,7 +130,7 @@ func (c *Coordinator) Heartb(arg *HbArg, rep *HbRep) error {
 		rep.Code = 1
 	} else {
 		w.Last = arg.Last
-		log.Printf("Heartb: %d", arg.Id)
+		// log.Printf("Heartb: %d", arg.Id)
 	}
 	return nil
 }
@@ -147,31 +157,83 @@ func (c *Coordinator) RegW(arg *RegWArg, rep *RegWRep) error {
 	c.workers.List = append(c.workers.List, Worker{Id: newId, Last: time.Now()})
 	rep.Id = newId
 	rep.NRed = c.nRed
+	rep.FCnt = c.fCnt
 	log.Printf("RegW: %s, nRed: %d", c.workers.Str(), rep.NRed)
 	return nil
+}
+
+func maxNum(ls []Task, kind int) int {
+	max := -1
+	for _, t := range ls {
+		if t.Type == kind && t.Num > max {
+			max = t.Num
+		}
+	}
+	return max
+}
+
+func findT(ls []Task, num int, kind int) int {
+	log.Printf("findT: num: %d, kind: %d", num, kind)
+	for i, t := range ls {
+		if t.Num == num && t.Type == kind {
+			return i
+		}
+	}
+	return -1
+}
+
+func last(ls []Task, kind int) int {
+	for i := len(ls) - 1; i >= 0; i -= 1 {
+		if ls[i].Type == kind {
+			log.Printf("last: %d", i)
+			return i
+		}
+	}
+	return -1
 }
 
 func (c *Coordinator) GetT(arg *GetTArg, rep *GetTRep) error {
 	c.tasks.mu.Lock()
 	defer c.tasks.mu.Unlock()
+	tList := c.tasks.List
+	if arg.DoneNum >= 0 && len(tList) > 0 {
+		i := findT(tList, arg.DoneNum, arg.DoneType)
+		if tList[i].Type == TaskR {
+			c.muRedCnt.Lock()
+			c.redCnt += 1
+			c.muRedCnt.Unlock()
+		}
+		last := last(tList, tList[i].Type)
+		tList[i] = tList[last]
+		tList[last] = tList[len(tList)-1]
+		tList = tList[:len(tList)-1]
+	}
+	log.Printf("GetT: tasks: %v", tList)
 	rep.Code = 1
-	for i, t := range c.tasks.List {
+	for i, t := range tList {
 		if t.Stat == TaskFree {
 			rep.Code = 0
 			rep.File = t.File
 			rep.Type = t.Type
 			rep.Num = t.Num
-			c.tasks.List[i].Stat = TaskLive // keep track
+			c.muRedCnt.Lock()
+			c.muRedCnt.Unlock()
+			log.Printf("GetT: rep: %v", rep)
+			tList[i].Stat = TaskLive // keep track
 			break
 		}
+	}
+	c.tasks.List = tList
+	if rep.Code == 1 && !c.Done() {
+		rep.Code = 2
 	}
 	return nil
 }
 
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
+	c := Coordinator{fCnt: len(files)}
 	c.tasks = new(Tasks)
-	c.tasks.fill(files)
+	c.tasks.fill(files, nReduce)
 	c.workers = new(Workers)
 	c.nRed = nReduce
 	go c.clean()
