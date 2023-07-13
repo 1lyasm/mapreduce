1d7dc09b55df52b96d2320f8a70c5cfc7e7d8960
1lyasm <il.mystafa@gmail.com>
2023-07-13 23:51:01 +0400

    add grep

--- src/mrapps/grep.go
+++ src/mrapps/grep.go
@@ -1,29 +1,30 @@
 package main
 
 import (
-	"bufio"
 	"mapreduce/mr"
 	"strings"
 )
 
-var pattern string = "abc"
+var pattern string = "lish"
 
 func Map(filename string, contents string) []mr.KeyValue {
-	kva := new([]mr.KeyValue)
-	reader := bufio.NewReader(strings.NewReader(contents))
-	// reader.ReadString('\n')
-	for {
-		s, e := reader.ReadString('\n')
-		if e != nil {
-			break
-		}
-		if strings.Contains(s, pattern) {
-			*kva = append(*kva, mr.KeyValue{Key: s})
+	kva := []mr.KeyValue{}
+	for _, line := range strings.Split(strings.TrimSuffix(contents, "\n"), "\n") {
+		if strings.Contains(line, pattern) {
+			kva = append(kva, mr.KeyValue{Key: line, Value: filename})
 		}
 	}
-	return *kva
+	return kva
 }
 
 func Reduce(key string, values []string) string {
-	return key
+	res := ""
+	for i, line := range values {
+		if i != len(values)-1 {
+			res += line + ","
+		} else {
+			res += line
+		}
+	}
+	return res
 }
--- src/run.sh
+++ src/run.sh
@@ -4,4 +4,4 @@ rm -r -f mr-* mr-out-*
 cd ../mrapps && go build -race -buildmode=plugin wc.go
 cd ../mrapps && go build -race -buildmode=plugin indexer.go
 cd ../mrapps && go build -race -buildmode=plugin grep.go
-cd ../main && go run -race mrcoordinator.go test.txt
+cd ../main && go run -race mrcoordinator.go pg-*.txt
