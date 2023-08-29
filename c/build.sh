2c91392e1be6a86ad7a0f25dbd8b8427bbf7cb05
1lyasm <il.mystafa@gmail.com>
2023-08-29 08:38:15 +0400

    Compile without -g

--- c/build.sh
+++ c/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-clang -g -O0 -Wall -Werror -std=c11 \
+clang -O2 -Wall -Werror -std=c11 \
     -fsanitize=address -Wno-declaration-after-statement \
     src/main/mrsequential.c src/mr/worker.c src/apps/wc.c -o mrsequential
