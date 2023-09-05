0ff4273ac80eb942b43cbc7521970ab17c2f4f86
1lyasm <il.mystafa@gmail.com>
2023-09-05 21:34:25 +0400

    Move implementation to .c

--- c/src/util/err.c
+++ c/src/util/err.c
@@ -1,10 +1,9 @@
+#include "err.h"
+
 #include <stdio.h>
 #include <stdlib.h>
-
-#include "err.h"
 
 void err(char *msg) {
     fprintf(stderr, "%s\n", msg);
     exit(EXIT_FAILURE);
 }
-
--- c/src/util/err.h
+++ c/src/util/err.h
@@ -1,12 +1,6 @@
 #ifndef ERR_H
 #define ERR_H
 
-#include <stdio.h>
-#include <stdlib.h>
-
-void err(char *msg) {
-  fprintf(stderr, "%s\n", msg);
-  exit(EXIT_FAILURE);
-}
+void err(char *msg);
 
 #endif
