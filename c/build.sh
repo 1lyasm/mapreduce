2828c4c6e3f4e7599a94fdea6495a5ff77341662
1lyasm <il.mystafa@gmail.com>
2023-09-08 02:42:45 +0400

    Compile mrcoordinator

--- c/build.sh
+++ c/build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
 mkdir -p bin/
-clang -O2 -Wall -Werror -Wpedantic -fsanitize=$2 \
-    src/main/mrsequential.c src/mr/mr.c src/apps/$1.c -o bin/mrsequential
+gcc -O2 -Wall -Werror -Wpedantic -fsanitize=$2 \
+    src/main/mrsequential.c src/mr/*.c src/util/*.c src/apps/$1.c -o bin/mrsequential
+gcc -O2 -Wall -Werror -Wpedantic -fsanitize=$2 \
+    src/main/mrcoordinator.c src/mr/*.c src/util/*.c -o bin/mrcoordinator
+
